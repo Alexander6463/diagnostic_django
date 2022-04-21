b0157ba972eafb4bc51037bb9a1795607668f6e5
aleksandr <aleksandr_drobnov@epam.com>
2022-04-21 23:36:16 +0300

    reader csv

--- csv_reader/views.py
+++ csv_reader/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 
 from .forms import UploadFileForm
 from csv import reader
-
+import tempfile
 
 # Create your views here.
 
@@ -11,7 +11,13 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            my_csv = reader(form.files['file'].read().decode("utf-8"))
+            tmp_file = tempfile.mktemp("upload.csv")
+            with open(tmp_file, "w") as f:
+                f.write(form.files['file'].read().decode("utf-8"))
+            with open(tmp_file, "r") as f:
+                for element in reader(f, delimiter=','):
+                    print(element)
+
 
 
             #handle_uploaded_file(request.FILES['file'])
